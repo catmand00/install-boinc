7ad982f55845f370b65e6b66490ab0579d9d8ace
Nicholas Offer <noffer@live.com>
2019-11-10 22:41:50 -0500

    maybe su not necessary

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "${WGC_ACCOUNT_KEY}" = "" ]; then
+if [ -z "${WGC_ACCOUNT_KEY}" ]; then
     echo "Need to set WGC_ACCOUNT_KEY"
     exit
 fi
@@ -13,8 +13,6 @@ systemctl enable boinc-client
 systemctl start boinc-client
 
 chmod g+r /var/lib/boinc/gui_rpc_auth.cfg
-usermod -a -G boinc $USER
-exec su $USER
 
 boinccmd --project_attach www.worldcommunitygrid.org "${WGC_ACCOUNT_KEY}"
 boinccmd --set_run_mode always
