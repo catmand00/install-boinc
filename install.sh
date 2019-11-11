8c7def8277d5690a1bacd0eec7a841665d7f9236
Nicholas Offer <noffer@live.com>
2019-11-10 22:26:31 -0500

    install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "${WGC_ACCOUNT_KEY}" = "" ]; then
+    echo "Need to set WGC_ACCOUNT_KEY"
+    exit
+fi
+
+# Assumes running as root (unsecured BOINC machines!)
+apt update
+apt install -y boinc-client boinctui
+
+systemctl enable boinc-client
+systemctl start boinc-client
+
+chmod g+r /var/lib/boinc/gui_rpc_auth.cfg
+usermod -a -G boinc $USER
+exec su $USER
+
+boinccmd --project_attach www.worldcommunitygrid.org "${WGC_ACCOUNT_KEY}"
+boinccmd --set_run_mode always
+boinccmd --set_network_mode always
