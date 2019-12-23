bb72d8fa7db1ed3b149107d0e09b462114cf1226
Nicholas Offer <noffer@live.com>
2019-12-23 10:16:03 -0500

    fix typo

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -z "${WGC_ACCOUNT_KEY}" ]; then
+if [ -z "${WCG_ACCOUNT_KEY}" ]; then
     echo "Need to set WGC_ACCOUNT_KEY"
     exit
 fi
@@ -13,7 +13,7 @@ apt install -y boinc-client boinctui
 echo -e "[Service]\nExecStart=\nExecStart=-/sbin/agetty --autologin root --noclear %I 38400 linux" >/etc/systemd/system/getty@tty1.service.d/override.conf
 
 # Set laptop lid to do nothing
-echo -e "HandleLidSwitch=ignore\nHandleLidSwitchExternalPower=ignore\nHandleLidSwitchDocked=ignore\n" >>/etc/systemd/logind.conf
+echo -e "HandleLidSwitch=ignore\nHandleLidSwitchExternalPower=ignore\nHandleLidSwitchDocked=ignore\n" >>/etc/systemd/logind.conF
 
 systemctl enable boinc-client
 systemctl start boinc-client
@@ -24,6 +24,6 @@ echo "Waiting for BOINC client..."
 sleep 5
 
 echo "Now attaching project"
-boinccmd --project_attach www.worldcommunitygrid.org "${WGC_ACCOUNT_KEY}"
+boinccmd --project_attach www.worldcommunitygrid.org "${WCG_ACCOUNT_KEY}"
 boinccmd --set_run_mode always
 boinccmd --set_network_mode always
